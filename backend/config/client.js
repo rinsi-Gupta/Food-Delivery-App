5f9f5aff977869af162cf8ec9bfdcc7442c18c7c
rinsi-Gupta <131142131+rinsi-Gupta@users.noreply.github.com>
2024-07-08 23:25:38 +0530

    Update client.js

--- backend/config/client.js
+++ backend/config/client.js
@@ -1,1 +1,2 @@
-export const CLIENT_DOMAIN = "http://localhost:5173";
+//export const CLIENT_DOMAIN = "http://localhost:5173";
+export const CLIENT_DOMAIN = "https://food-delivery-app-1-jx7z.onrender.com";
